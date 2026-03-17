372ca169e40cd95b93b4feb1978ded7fac9ab7ac
Kristy Heijenk <ksheijenk@hotmail.com>
2026-03-17 15:19:47 +0100

    Added exercises 1.

--- src/main/java/org/exercisesoop/BankAccount.java
+++ src/main/java/org/exercisesoop/BankAccount.java
@@ -1,4 +1,61 @@
 package org.exercisesoop;
 
 public class BankAccount {
+
+    private String accountHolder;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String accountHolder, double initialBalance) {
+        if (accountHolder == null || accountHolder.isEmpty()) {
+            throw new IllegalArgumentException("Account holder cannot be empty");
+        }
+
+        this.accountHolder = accountHolder;
+
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+        } else {
+            this.balance = 0;
+            System.out.println("Initial balance cannot be negative. Set to 0.");
+        }
+    }
+
+    // Getter
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setter (only for name!)
+    public void setAccountHolder(String accountHolder) {
+        if (accountHolder == null || accountHolder.isEmpty()) {
+            throw new IllegalArgumentException("Account holder cannot be empty");
+        }
+        this.accountHolder = accountHolder;
+    }
+
+    // Deposit
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Deposit must be positive");
+        }
+    }
+
+    // Withdraw
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdraw must be positive");
+        } else if (amount > balance) {
+            System.out.println(accountHolder + " has insufficient funds. Withdrawal of "
+                    + amount + " failed.");
+        } else {
+            balance -= amount;
+        }
+    }
 }
--- src/main/java/org/exercisesoop/Main.java
+++ src/main/java/org/exercisesoop/Main.java
@@ -1,7 +1,19 @@
 package org.exercisesoop;
 
 public class Main {
-    static void main() {
 
+    public static void main(String[] args) {
+
+        BankAccount account1 = new BankAccount("Leonard", 6000);
+        BankAccount account2 = new BankAccount("Penny", 100);
+
+        account1.deposit(600);
+        account1.withdraw(100);
+
+        account2.deposit(100);
+        account2.withdraw(250);
+
+        System.out.println(account1.getAccountHolder() + " balance: " + account1.getBalance());
+        System.out.println(account2.getAccountHolder() + " balance: " + account2.getBalance());
     }
 }
